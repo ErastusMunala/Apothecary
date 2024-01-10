67a289a81102503aac0cfaa16b8adc29fac84dd8
Brianmanoti <brianmanoti254@gmail.com>
2024-01-11 02:03:44 +0300

    Pharma Project Final Submission and Merging as a Django Programme

--- pharma/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Product:
-    def __init__(self, image:str, name: str, cost: int, quantity: int):
-        self.image = image
-        self.name = name
-        self.cost = cost
-        self.quantity = quantity
